f69d60a8275f2f1a7ada841e24f30ab85fc7267e
25SUB5935_U20 <25sub5935_u20@rpsconsulting.in>
2026-05-15 11:23:42 +0530

    removed test file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
 
-@app.route("/")
-def hello():f
-    return "HEllo, Jenkins CI/CD from Flask App on Windows!"
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "HELLO, Jenkins CI/CD from Flask App on Windows!"
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
